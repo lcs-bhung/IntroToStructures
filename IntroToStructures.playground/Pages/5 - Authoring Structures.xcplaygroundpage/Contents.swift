71b975b2df97451e51897f2c17a2451ab892a6c5
Branton Hung <BHung@lcs.on.ca>
2020-02-19 12:01:33 -0500

    Finished the third one

--- IntroToStructures.playground/Pages/5 - Authoring Structures.xcplaygroundpage/Contents.swift
+++ IntroToStructures.playground/Pages/5 - Authoring Structures.xcplaygroundpage/Contents.swift
@@ -33,6 +33,8 @@ import Foundation
  */
 // Begin your work here...
 
+
+//Pow and pi
 struct circle {
     //Mark: Properties
     var radius: Double
@@ -52,8 +54,63 @@ struct circle {
 
 //Create circle instance
 let someCircle = circle(radius: 6.0)
-
 print (someCircle.areaDescription)
+
+
+//Fraction
+struct triangle {
+    //Mark: Properties
+    var base: Double
+    var height: Double
+    var a : Int
+    var b : Int
+    var c : Int
+    //Area of the triangle
+    var area: Double {
+        return 1/2 * base * height
+    }
+    //Perimeter of the triangle
+    var perimtere: Int {
+       return a + b + b
+    }
+    //Compound property to a string
+    var areaDescription: String {
+        "The area of the triangle is \(area) square units, and the perimeter of the triangle is \(perimtere) units"
+    }
+}
+
+//create the triangle intance
+let someTriangle = triangle(base: 20.0, height: 15.5, a: 9, b: 10, c: 6)
+print (someTriangle.areaDescription)
+
+
+//Pow, fraction and Pie
+struct sphere {
+    //Mark: Properties
+    var radius: Double
+    //Area of the sphere
+    var area: Double {
+        return (4 * Double.pi * pow(radius, 3))/3
+    }
+    //Surface area of the sphere
+    var surfaceArea: Double {
+        return 4*Double.pi * pow(radius, 2)
+    }
+    //Compound property into a string
+    var areaDescription: String {
+        "The area of the sphere is \(area) square units, the surface area of the sphere is \(surfaceArea) units"
+    }
+}
+
+//Create the sphere instance
+let someSphere = sphere(radius: 10.5)
+print (someSphere.areaDescription)
+
+
+
+
+
+
 
 /*:
  [Previous: Area Example - Rectangle](@previous) | Page 4
